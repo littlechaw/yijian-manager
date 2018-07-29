21d4c9a94fd0966bf7711b7dc2099784c95fb71b
chaw <wangchao@tan66.com>
2018-07-30 00:49:32 +0800

    pages

--- src/views/FinancialFlow.vue
+++ src/views/FinancialFlow.vue
@@ -26,24 +26,27 @@
         border
         :header-cell-style="headerStyle"
         style="width: 100%;text-align:center">
-        <el-table-column prop="monitorDynamicRegisterCount" label="商家ID"></el-table-column>
-        <el-table-column prop="monitorDynamicAuthInfoCount" label="店名"></el-table-column>
-        <el-table-column prop="monitorDynamicAuthBankCount" label="联系人"></el-table-column>
-        <el-table-column prop="monitorDynamicApplyCount" label="申请人"></el-table-column>
-        <el-table-column prop="monitorDynamicApplyPCT" label="状态"></el-table-column>
-        <el-table-column prop="monitorDynamicApplyPCT" label="申请时间"></el-table-column>
-        <el-table-column prop="monitorDynamicApplyPCT" label="操作">
-          <template slot-scope="scope">
-            <el-button @click="handleClick(scope.row)" type="text" size="small">查看商家信息</el-button>
-          </template>
-        </el-table-column>
+        <el-table-column prop="outTradeNo" label="订单编号"></el-table-column>
+        <el-table-column prop="name" label="店名"></el-table-column>
+        <el-table-column prop="price" label="结算金额"></el-table-column>
+        <el-table-column prop="profit" label="抽成"></el-table-column>
+        <el-table-column prop="createTime" label="订单结算时间"></el-table-column>
       </el-table>
+      <div class="block">
+        <el-pagination
+          @current-change="handleCurrentChange"
+          :page-size="10"
+          layout="prev, pager, next, jumper"
+          :total="total">
+        </el-pagination>
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
+  import {getBeforeDays} from "../utils/mUtils";
+
   export default {
     name: "FinancialFlow",
     data() {
@@ -53,15 +56,38 @@
           storeName: '',
           searchDate: ''
         },
-        tableData: []
+        tableData: [],
+        total: 5,
+        currentPage: 1
       }
     },
     mounted() {
       this.searchData.searchDate = [getBeforeDays(7), new Date()];
+      this.queryData();
     },
     methods: {
       queryData() {
-
+        let url = '/yijian/opRoot/findServiceFlow.do';
+        let outTradeNo = this.searchData.orderID ? this.searchData.orderID : 0,
+          name = this.searchData.storeName,
+          startIndex = this.currentPage == 1 ? 0 : this.currentPage * 10 - 1,
+          pageSize = 10,
+          createTimeStart = this.$transferDate(this.searchData.searchDate[0]),
+          createTimeEnd = this.$transferDate(this.searchData.searchDate[1]);
+        let data = {
+          outTradeNo,
+          name,
+          startIndex,
+          pageSize,
+          createTimeStart,
+          createTimeEnd
+        };
+        this.$axios.dopost(url, data).then(res => {
+          this.tableData = res;
+          this.total = res.length > 0 ? res.length : 1;
+        }).catch(e => {
+          this.$showErrorMessage(this, e);
+        })
       },
       headerStyle: function () {
         return {
@@ -72,6 +98,14 @@
       },
       handleClick(d) {
 
+      },
+      handleCurrentChange(val) {
+        this.currentPage = val;
+      }
+    },
+    watch: {
+      currentPage() {
+        this.queryData();
       }
     }
   }
